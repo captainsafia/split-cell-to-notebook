4c2ffebb36c33cec2332cb3b8ca59b0cc7b35233
Safia Abdalla <safia@safia.rocks>
2016-03-18 12:01:17 -0400

    Removed nesting in promises

--- split-cell-to-notebook.js
+++ split-cell-to-notebook.js
@@ -8,29 +8,29 @@ define(['base/js/namespace', 'base/js/utils', 'jquery'], function(Jupyter, utils
         var parent_path = utils.url_path_split(nb_path)[0];
         
         Jupyter.notebook.contents.copy(nb_path, parent_path).then(function(data) {
-            Jupyter.notebook.contents.get(data.path, {type: 'notebook'}).then(function(data) {
-                Jupyter.notebook.fromJSON(data);
-                var copy_selected_cell = Jupyter.notebook.get_selected_cell();
-                copy_selected_cell.set_text(content_before_cursor);
-                console.log(copy_selected_cell.get_text());
-                Jupyter.notebook.save_notebook().then(function() {
-                    console.log(Jupyter.notebook);
-                    console.log('Notebook saved!');
-                });
-            });
+            return Jupyter.notebook.contents.get(data.path, {type: 'notebook'})
+        }).then(function(data) {
+            Jupyter.notebook.fromJSON(data);
+            var copy_selected_cell = Jupyter.notebook.get_selected_cell();
+            copy_selected_cell.set_text(content_before_cursor);
+            console.log(copy_selected_cell.get_text());
+            return Jupyter.notebook.save_notebook();
+        }).then(function() {
+                console.log(Jupyter.notebook);
+                console.log('Notebook saved!');
         });
 
         Jupyter.notebook.contents.copy(nb_path, parent_path).then(function(data) {
-            Jupyter.notebook.contents.get(data.path, {type: 'notebook'}).then(function(data) {
-                Jupyter.notebook.fromJSON(data);
-                var original_selected_cell = Jupyter.notebook.get_selected_cell();
-                original_selected_cell.set_text(content_after_cursor);
-                console.log(original_selected_cell.get_text());
-                Jupyter.notebook.save_notebook().then(function() {
-                    console.log(Jupyter.notebook);
-                    console.log('Notebook saved!');
-                });
-            });
+            return Jupyter.notebook.contents.get(data.path, {type: 'notebook'});
+        }).then(function(data) {
+            Jupyter.notebook.fromJSON(data);
+            var original_selected_cell = Jupyter.notebook.get_selected_cell();
+            original_selected_cell.set_text(content_after_cursor);
+            console.log(original_selected_cell.get_text());
+            return Jupyter.notebook.save_notebook();
+        }).then(function() {
+            console.log(Jupyter.notebook);
+            console.log('Notebook saved!');
         });
     }
 
